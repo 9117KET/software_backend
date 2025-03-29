e4405e8302327b4c7d8b43e79db406e4f1b0715b
Kinlo Ephriam Tangiri (KET) <kinlotangiri911@gmail.com>
2025-03-29 09:52:12 +0100

    secured the environment variables

--- run.bat
+++ run.bat
@@ -1,11 +1,8 @@
 @echo off
-set SPRING_DATASOURCE_USERNAME=postgres
-set SPRING_DATASOURCE_PASSWORD=PzvtlSLIhZUZdSNzJsPvfxzlYqCPzbqu
-set SPRING_DATASOURCE_URL=jdbc:postgresql://maglev.proxy.rlwy.net:44972/railway
-set SPRING_MAIL_HOST=smtp.gmail.com
-set SPRING_MAIL_PORT=587
-set SPRING_MAIL_USERNAME=dashpress1000@gmail.com
-set SPRING_MAIL_PASSWORD=hifkcdnflwecfpvj
-set JWT_SECRET=b8iuBAt4rXDi4MI863UWRFKR0q4KBT7I6jU1maMuyz8G
+REM Load environment variables from .env file
+for /f "tokens=*" %%a in (.env) do (
+    set %%a
+)
 
+REM Run the Spring Boot application
 call mvnw.cmd spring-boot:run 
